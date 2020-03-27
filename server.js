b1c22c11db63875103cc673d6599cfc8c7ce2723
sunhong6 <hongyi.sun@mail.utoronto.ca>
2020-03-27 17:27:22 -0400

    added env checker

--- server.js
+++ server.js
@@ -58,6 +58,14 @@ const sessionChecker = (req, res, next) => {
     }    
 };
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static('frontend/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+	})
+}
+
 // A route to login and create a session
 app.post('/users/login', (req, res) => {
 	const email = req.body.email
