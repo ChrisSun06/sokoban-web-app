c80ff8de1a365c7b8fedda3d90312c32e112194a
sunhong6 <hongyi.sun@mail.utoronto.ca>
2020-03-27 17:31:39 -0400

    build

---
+++ frontend/build/precache-manifest.c0ffdcc1d5c49d4c3e8087f25eed2ee0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e22bc365489deb52d169152d903b1b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59ede736ac089f668fe7",
+    "url": "/static/css/main.fb6c335f.chunk.css"
+  },
+  {
+    "revision": "c1961f7c3e7ddbe93f45",
+    "url": "/static/js/2.6e33a67e.chunk.js"
+  },
+  {
+    "revision": "6e761d6093a093130b7678061745c64b",
+    "url": "/static/js/2.6e33a67e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ede736ac089f668fe7",
+    "url": "/static/js/main.816fa796.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "d9bb412af53e76a51600d92403ff3f4d",
+    "url": "/static/media/admin.d9bb412a.jpg"
+  },
+  {
+    "revision": "50b8c68f8daad38f2e1d33d31f8f27aa",
+    "url": "/static/media/b.50b8c68f.jpg"
+  },
+  {
+    "revision": "577b1de90933f7bf53c06ab3f5c6160b",
+    "url": "/static/media/b.577b1de9.jpg"
+  },
+  {
+    "revision": "c85b5009cef69e3bfa38ad8489cbe1e1",
+    "url": "/static/media/b.c85b5009.jpg"
+  },
+  {
+    "revision": "519fb9e1725f11dcb099f2c8b5a9217c",
+    "url": "/static/media/bag.519fb9e1.png"
+  },
+  {
+    "revision": "52a3346c86e69ca22f1f3f4ac771c2cb",
+    "url": "/static/media/d.52a3346c.jpg"
+  },
+  {
+    "revision": "0570c96463234bfb424ac79c906bcbd8",
+    "url": "/static/media/g1.0570c964.png"
+  },
+  {
+    "revision": "647c8bdb4f69460f4212dd9e46fe7eca",
+    "url": "/static/media/l.647c8bdb.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "647c8bdb4f69460f4212dd9e46fe7eca",
+    "url": "/static/media/s.647c8bdb.jpg"
+  },
+  {
+    "revision": "0570c96463234bfb424ac79c906bcbd8",
+    "url": "/static/media/sokobanpreview1.0570c964.png"
+  },
+  {
+    "revision": "9e0c6a4685a645a40b261b78e6766bae",
+    "url": "/static/media/sokobanpreview2.9e0c6a46.jpeg"
+  }
+]);
