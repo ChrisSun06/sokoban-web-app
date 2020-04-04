182b4728eaa23e669f7f6285a9bebddeabfc10ed
sunhong6 <hongyi.sun@mail.utoronto.ca>
2020-04-04 17:30:46 -0400

    finished2

--- server.js
+++ server.js
@@ -445,12 +445,12 @@ io.on('connection', function (socket) {
 
 
 
-// app.use(express.static(__dirname + "/client/build"));
+app.use(express.static(__dirname + "/client/build"));
 
-// // All routes other than above will go to index.html
-// app.get("*", (req, res) => {
-//     res.sendFile(__dirname + "/client/build/index.html");
-// });
+// All routes other than above will go to index.html
+app.get("*", (req, res) => {
+    res.sendFile(__dirname + "/client/build/index.html");
+});
 
 /*************************************************/
 // Express server listening...
